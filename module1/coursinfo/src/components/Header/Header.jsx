e423d257e1f5bb0fd13450ba1f395fe18fe91f6b
edufalcao14 <eduardo.sampaio@student.vinci.be>
2024-09-17 15:24:36 +0200

    1.1-1.2 : import image

--- module1/coursinfo/src/components/Header/Header.jsx
+++ module1/coursinfo/src/components/Header/Header.jsx
@@ -1,8 +1,13 @@
+const Header = ({ course }) => {
+  return (
+    <div>
+      <h1>{course}</h1>
+      <img
+        src="https://moodle.vinci.be/pluginfile.php/1/core_admin/logo/0x150/1612973627/LOGO%20HE%20VINCI.png"
+        alt="logo vinci"
+      />
+    </div>
+  );
+};
 
-const Header = ({course}) => {
-    return (
-    <h1>{course}</h1>
-    )
-  }
-
-export default Header
+export default Header;
